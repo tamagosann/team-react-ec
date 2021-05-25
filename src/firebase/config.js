86ea575ebf2466ea147488186abda522f58b40df
tamagosann <hssvs619@gmail.com>
2021-05-26 00:30:09 +0900

    最低限の初期設定

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAKX_OBusnzckWhihXNNGszWLslGkxY7C0",
+    authDomain: "team-react-ec.firebaseapp.com",
+    projectId: "team-react-ec",
+    storageBucket: "team-react-ec.appspot.com",
+    messagingSenderId: "799034838891",
+    appId: "1:799034838891:web:4ac7332a9119423437c62e",
+    measurementId: "G-L64N1N2CQ8"
+  };
